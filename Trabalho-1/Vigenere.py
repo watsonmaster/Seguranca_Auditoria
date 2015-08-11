f8db018454c5070c2dd0d488cd6ce3eabc3ea7f3
Watson Vieira Costa Junior <watsonmaster@gmail.com>
2015-08-10 23:59:13 -0300

    Testado e Verificado Ajustes
    Marcado ajustes necessários.

--- Trabalho-1/Vigenere.py
+++ Trabalho-1/Vigenere.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #Program de Criptografia Cifra de Vigenère
+#TO-DO fazer a parte de giro de valores acima de 255
 nome = raw_input("Digite um valor a ser cryptografado\n")
 list_cypt = list(nome)
 key = list(raw_input("Digite a Chave de Codificação\n"))
@@ -12,15 +13,17 @@ for x in range(0, len(list_cypt), 1):
   #list_cypt[x] = chr(ord(nome[x]) + ord(key[key_index]))
   #print("Chave é numero: " + str(key_index))
   if (key_index < len(key)):
-  	val_key = ord(key[key_index])
-  	list_cypt[x] = chr(ord(nome[x]) + val_key)
-  	key_index = key_index + 1
-  	#print("somou valor " + str(key_index))
+    val_key = ord(key[key_index])
+    #print(ord(nome[x]) + val_key)
+    #print(nome[x])
+    list_cypt[x] = chr(ord(nome[x]) + val_key)
+    key_index = key_index + 1
+    #print("somou valor " + str(key_index))
   else:
-  	key_index = 0
-  	val_key = ord(key[key_index])
-  	list_cypt[x] = chr(ord(nome[x]) + val_key)
-  	#print("Zerou")
+    key_index = 0
+    val_key = ord(key[key_index])
+    list_cypt[x] = chr(ord(nome[x]) + val_key)
+    #print("Zerou")
 
 print(list_cypt)
 print(''.join(list_cypt))
@@ -28,14 +31,14 @@ print(''.join(list_cypt))
 list_plain = list_cypt
 key_index = 0
 for x in range(0, len(list_cypt), 1):
-	if (key_index < len(key)):
-		val_key = ord(key[key_index])
-		list_plain[x] = chr(ord(list_plain[x]) - val_key)
-		key_index = key_index + 1
-  	#print("somou valor " + str(key_index))
-  	else:
-  		key_index = 0
-  		val_key = ord(key[key_index])
-  		list_plain[x] = chr(ord(list_plain[x]) - val_key)
+  if (key_index < len(key)):
+    val_key = ord(key[key_index])
+    list_plain[x] = chr(ord(list_plain[x]) - val_key)
+    key_index = key_index + 1
+    #print("somou valor " + str(key_index))
+  else:
+    key_index = 0
+    val_key = ord(key[key_index])
+    list_plain[x] = chr(ord(list_plain[x]) - val_key)
 
 print(''.join(list_plain))
